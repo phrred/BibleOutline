21c7c58b4786642353103981c8fc065e18463b4a
Fred Hong <fredhong@google.com>
2026-08-17 22:18:54 -0400

    Configure Firebase project credentials (bible-outline-f10cc) & cloud sync

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -2,12 +2,13 @@
 // Replace these values with your Firebase Project config from console.firebase.google.com
 
 export const FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAn_aeb_sHY7sToqlLeXrJAHBtf5Hlp4uc",
+  authDomain: "bible-outline-f10cc.firebaseapp.com",
+  projectId: "bible-outline-f10cc",
+  storageBucket: "bible-outline-f10cc.firebasestorage.app",
+  messagingSenderId: "74158682451",
+  appId: "1:74158682451:web:8aef02018d3ada0b9831a6",
+  measurementId: "G-SVT2LTW7R4"
 };
 
 // Check if Firebase credentials have been configured
